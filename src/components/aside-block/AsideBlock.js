4e1348388427e5c2f5b14edeea76575d3d85b31d
Catherine358 <58996148+Catherine358@users.noreply.github.com>
2020-04-22 12:06:52 +0300

    added day picker but not customized it

--- src/components/aside-block/AsideBlock.js
+++ src/components/aside-block/AsideBlock.js
@@ -1,13 +1,15 @@
 import React from "react";
-import Calendar from "./calendar";
 import Grid from '@material-ui/core/Grid';
 import EventsAside from "./events";
+import DayPicker from "react-day-picker";
+import './day-picker.less';
 
 const AsideBlock = (props) => {
+    const WEEKDAYS_SHORT = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
     return (
         <Grid container direction="column" justify="center" className="aside-container">
             <div className="title">Calendar</div>
-            <Calendar/>
+            <DayPicker weekdaysShort={WEEKDAYS_SHORT}/>
             <div className="title">Upcoming events</div>
             <EventsAside/>
         </Grid>
