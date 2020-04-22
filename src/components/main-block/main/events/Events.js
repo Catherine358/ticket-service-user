1e1822ad0f345616d3f6ae316332fde66b79c3e8
Catherine358 <58996148+Catherine358@users.noreply.github.com>
2020-04-22 10:06:12 +0300

    added buttons to go through pages

--- src/components/main-block/main/events/Events.js
+++ src/components/main-block/main/events/Events.js
@@ -2,9 +2,16 @@ import React, { useState, useEffect } from "react";
 import './events.less';
 import { requestEvents } from "../../../services";
 import Event from "./Event";
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
+
+const updatePage = (indexForPagination, quantity) => {
+    return indexForPagination + 4 * quantity;
+};
 
 const Events = (props) => {
     const [events, setEvents] = useState([]);
+    const [indexForPagination, setIndex] = useState(0);
 
     useEffect(() => {
         async function fetchEvents() {
@@ -32,7 +39,17 @@ const Events = (props) => {
                 </select>
             </div>
             <div className="list">
-                <Event arr={events} indexForPagination={0}/>
+                <Event arr={events} indexForPagination={indexForPagination}/>
+                <Grid container direction="row" className="btn-page-container w-100">
+                    <Grid container item justify="center">
+                        <Button variant="contained" className="btn-page-prev" onClick={() => {
+                            setIndex(updatePage(indexForPagination, -1));
+                        }} disabled={indexForPagination === 0}>PREV</Button>
+                        <Button variant="contained" className="btn-page-next" onClick={() => {
+                            setIndex(updatePage(indexForPagination, 1));
+                        }} disabled={indexForPagination + 4 >= events.length}>NEXT</Button>
+                    </Grid>
+                </Grid>
             </div>
         </div>
     )
