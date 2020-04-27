33d487147700ff85ea5edc4139c742ea26afb10c
PanRuslan <ru.ganiyev@gmail.com>
2020-04-27 16:55:46 +0300

    added checkbox to shopping cart

--- src/components/main-block/main/shopping-cart/ShoppingCart.js
+++ src/components/main-block/main/shopping-cart/ShoppingCart.js
@@ -13,16 +13,16 @@ const ShoppingCart = (props) => {
                     The tickets shown here have now been reserved for you for 10 minutes.
                 </div>
             </div>
-            <div className="row w-100 border-cart p-4 m-0">
+            <div className="row w-100 border-cart p-2 m-0 mr-2">
                 <div>
-                    <p className="border-cart-header ml-md-3 mx-0 text-md-left text-center">
+                    <p className="border-cart-header ml-md-3 mx-0 mt-0 text-md-left text-center">
                         Lady Gaga | World tour 2020 | 14 June 2019
                     </p>
-                    <div className="col-md-9 col-7 d-flex justify-content-between">
+                    <div className="col-7 pl-0 d-flex justify-content-between">
                         <span>Row</span>
                         <span>Place</span>
                     </div>
-                    <div className="row justify-content-between seat-cart-row">
+                    <div className="row ml-0 mr-0 justify-content-between seat-cart-row">
                         <span className="seat-cart-font">8</span>
                         <span className="seat-cart-font">11R</span>
                         <span className="seat-cart-cross">&times;</span>
@@ -32,9 +32,9 @@ const ShoppingCart = (props) => {
                         <span className="seat-cart">&#8364; 0</span>
                     </div>
                     <Button variant="contained" className="shopping-cart-btn col-md-4 col-12">PAY</Button>
-                    <input required type="checkbox" id="agreement" name="agreement" title="This is required"/>
                     <label form="agreement" className="label-for-checkbox pos-cart-agreement">
-                        <span/>
+                        <input required type="checkbox" id="agreement" name="agreement" title="This is required"/>
+                        <span className="checkmark"/>
                         <span className="red-text">*</span>
                         I have read the
                         <span className="yellow-text"> Terms and Conditions </span>
