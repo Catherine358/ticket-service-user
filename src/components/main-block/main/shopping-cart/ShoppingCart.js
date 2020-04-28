26b250b2831d7f37d021b9ccdd373e7e864eda30
PanRuslan <ru.ganiyev@gmail.com>
2020-04-28 15:51:42 +0300

    Fixed required checkbox

--- src/components/main-block/main/shopping-cart/ShoppingCart.js
+++ src/components/main-block/main/shopping-cart/ShoppingCart.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { withRouter } from "react-router";
 import './shoppingCart.less';
 import Button from "@material-ui/core/Button";
 import {Link} from "react-router-dom";
@@ -19,7 +20,7 @@ const TicketsInCart = ({ lockedSeats, priceRanges, dispatch }) => {
     let ticket = lockedSeats.map(data => {
         let arr = data.split("-");
         return (
-            <div key={data} className="row justify-content-between seat-cart-row">
+            <div key={data} className="row justify-content-between seat-cart-row ml-0">
                 <span className="seat-cart-font">{arr[0]}</span>
                 <span className="seat-cart-font">{arr[1]}</span>
                 <span className="seat-cart-cross" onClick={() => {
@@ -86,6 +87,32 @@ const ShoppingCart = (props) => {
         );
     }
 
+    const goToPay = (event) => {
+        event.preventDefault();
+        let lockedSeats = [];
+        let map = new Map();
+        ticketsInCart.forEach(data => {
+            let arr = data.split("-");
+            if(map.get(arr[0]) === undefined){
+                let seats = [];
+                seats.push(arr[1]);
+                map.set(arr[0], seats);
+            }else{
+                let tmp = map.get(arr[0]);
+                tmp.push(arr[1]);
+                map.set(arr[0], tmp);
+            }
+        });
+        map.forEach((value, key) => {
+            lockedSeats.push({
+                row: key.toString(),
+                seats: value
+            });
+        });
+        localStorage.setItem("lockedSeats", JSON.stringify(lockedSeats));
+        props.history.push("/payment");
+    };
+
     return (
         <div className="shopping-cart">
             <div className="shopping-cart-header">
@@ -108,41 +135,19 @@ const ShoppingCart = (props) => {
                     <TicketsInCart lockedSeats={ticketsInCart} priceRanges={priceRanges.priceRanges}
                                    dispatch={dispatch}/>
                     <PricesSum pricesSum={pricesSum} ticketsCount={ticketsCount}/>
-                    <Link to={"/payment"}>
-                        <Button variant="contained" className="shopping-cart-btn col-md-4 col-12" onClick={() => {
-                            let lockedSeats = [];
-                            let map = new Map();
-                            ticketsInCart.forEach(data => {
-                                let arr = data.split("-");
-                                if(map.get(arr[0]) === undefined){
-                                    let seats = [];
-                                    seats.push(arr[1]);
-                                    map.set(arr[0], seats);
-                                }else{
-                                    let tmp = map.get(arr[0]);
-                                    tmp.push(arr[1]);
-                                    map.set(arr[0], tmp);
-                                }
-                            });
-                            map.forEach((value, key) => {
-                                lockedSeats.push({
-                                    row: key.toString(),
-                                    seats: value
-                                });
-                            });
-                            localStorage.setItem("lockedSeats", JSON.stringify(lockedSeats));
-                        }}>PAY</Button>
-                    </Link>
-                    <label form="agreement" className="label-for-checkbox pos-cart-agreement">
-                        <input required type="checkbox" id="agreement" name="agreement" title="This is required"/>
-                        <span className="checkmark"/>
-                        <span className="red-text">*</span>
-                        I have read the
-                        <Link to={"/terms"}>
-                            <span className="yellow-text"> Terms and Conditions </span>
-                        </Link>
-                        and fully agree with them.
-                    </label>
+                    <form onSubmit={goToPay}>
+                        <Button type="submit" variant="contained" className="shopping-cart-btn col-md-4 col-12">PAY</Button>
+                        <label form="agreement" className="label-for-checkbox pos-cart-agreement">
+                            <input required type="checkbox" id="agreement" name="agreement" title="This is required" />
+                            <span className="checkmark"/>
+                            <span className="red-text">*</span>
+                            I have read the
+                            <Link to={"/terms"}>
+                                <span className="yellow-text"> Terms and Conditions </span>
+                            </Link>
+                            and fully agree with them.
+                        </label>
+                    </form>
                     <div className="clear-cart" onClick={() => {
                         localStorage.removeItem("lockedSeats");
                         setSection(0);
@@ -153,4 +158,4 @@ const ShoppingCart = (props) => {
     );
 };
 
-export default ShoppingCart;
+export default withRouter(ShoppingCart);
