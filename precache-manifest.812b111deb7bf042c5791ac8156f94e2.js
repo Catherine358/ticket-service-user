aa4865fcd00311c58202ef3db83687d0b44b71ea
Catherine358 <58996148+Catherine358@users.noreply.github.com>
2020-05-14 18:13:25 +0300

    Updates

---
+++ precache-manifest.812b111deb7bf042c5791ac8156f94e2.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf6ee2a292aa4417c55adbea2610304",
+    "url": "/ticket-service-user/index.html"
+  },
+  {
+    "revision": "bd51865c06ef165a3c53",
+    "url": "/ticket-service-user/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "d8cf80a658f3899e8c68",
+    "url": "/ticket-service-user/static/css/main.5af12519.chunk.css"
+  },
+  {
+    "revision": "bd51865c06ef165a3c53",
+    "url": "/ticket-service-user/static/js/2.4acd52b9.chunk.js"
+  },
+  {
+    "revision": "2284a65b309d84a32bee504661ef47a7",
+    "url": "/ticket-service-user/static/js/2.4acd52b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8cf80a658f3899e8c68",
+    "url": "/ticket-service-user/static/js/main.253a991a.chunk.js"
+  },
+  {
+    "revision": "17bb4c02596a9b77f3aa",
+    "url": "/ticket-service-user/static/js/runtime-main.7b0666fe.js"
+  },
+  {
+    "revision": "9b609f6cc20e5e494d7cab55ed81f97a",
+    "url": "/ticket-service-user/static/media/DINPro-Black.9b609f6c.otf"
+  },
+  {
+    "revision": "7b4d603d7e0b73bd931e0b7eb50214ce",
+    "url": "/ticket-service-user/static/media/DINPro-CondBold.7b4d603d.otf"
+  },
+  {
+    "revision": "e0be9bd5e372b2c379893b776e7b2b96",
+    "url": "/ticket-service-user/static/media/DINPro-Light.e0be9bd5.otf"
+  },
+  {
+    "revision": "86fda98796bb4100ba905535a77723bd",
+    "url": "/ticket-service-user/static/media/DINPro-Regular.86fda987.otf"
+  },
+  {
+    "revision": "9e21c870d34ce26ecf0ea3102c939d2f",
+    "url": "/ticket-service-user/static/media/FUTURAFUTURIS-BOLD.9e21c870.OTF"
+  },
+  {
+    "revision": "ef2b9b2c197efa3ab2ff6ffcc7d47fa7",
+    "url": "/ticket-service-user/static/media/FUTURAFUTURIS.ef2b9b2c.OTF"
+  },
+  {
+    "revision": "4cf3749e3deaec2ac62519cc289f4735",
+    "url": "/ticket-service-user/static/media/TicketDesign.4cf3749e.jpg"
+  },
+  {
+    "revision": "867bd061ee8be461dcb80b42c8cb6446",
+    "url": "/ticket-service-user/static/media/big_hall.867bd061.png"
+  },
+  {
+    "revision": "803be05eadb68d32561edb1c6a84229b",
+    "url": "/ticket-service-user/static/media/cover.803be05e.jpg"
+  },
+  {
+    "revision": "45b810365f827f73604801de71145ef0",
+    "url": "/ticket-service-user/static/media/icon-rss.45b81036.png"
+  },
+  {
+    "revision": "b9c84379604adca6e6e7b94f85dd0294",
+    "url": "/ticket-service-user/static/media/icon-utube.b9c84379.png"
+  },
+  {
+    "revision": "4fa451cb9c4bfe054982ad2eb6468b98",
+    "url": "/ticket-service-user/static/media/logo_col.4fa451cb.svg"
+  },
+  {
+    "revision": "647c99c9f582f1b874caa1564a130d2e",
+    "url": "/ticket-service-user/static/media/logo_white.647c99c9.svg"
+  },
+  {
+    "revision": "8ed2b7ed6f68967ba483f3404480108d",
+    "url": "/ticket-service-user/static/media/rhwk_decorGrey.8ed2b7ed.png"
+  },
+  {
+    "revision": "0181df38de523152a539cbcd1960b1ca",
+    "url": "/ticket-service-user/static/media/small_hall.0181df38.png"
+  }
+]);
